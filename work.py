f836cf34382c1cc1a4284fb2ff07b2ad058a85a4
Talon <talonpollard@gmail.com>
2019-10-16 16:02:02 -0700

    Read html file + find entry data

--- work.py
+++ work.py
@@ -1,7 +1,18 @@
-from bs4 import BeautifulSoup as bs
-import requests
+from bs4 import BeautifulSoup as bs # webscrape
+import requests # websites
+import codecs # files
 
-page = requests.get("http://citemgr/citemgr/violation_trans_main.php?cite_array=&cite_sysid=83813&cite_number=1306-25673")
+# request website
+# page = requests.get("http://citemgr/citemgr/violation_trans_main.php?cite_array=&cite_sysid=83813&cite_number=1306-25673")
 
-soup = bs(page.content, features="html.parser")
-print(soup.prettify())
+f = codecs.open("citemgr_ex1.html", "r", "utf-8")
+
+soup = bs(f.read(), "html.parser")
+
+print(soup.find("td", class_="tblkeypcs").a.get_text()) # Cite ID
+for i in soup.find_all("td", class_="tblpcs"):
+    print(i.get_text())
+
+
+
+    
