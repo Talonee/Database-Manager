64fac06a307defd00c473c9af4807b194a00de33
Talon <talonpollard@gmail.com>
2019-10-16 21:29:25 -0700

    Successfully scrape entries for 6/8 columns

--- work.py
+++ work.py
@@ -34,7 +34,8 @@ def init_excel():
         "License Number": [],
         "Registered Owner": [],
         "Status": [],
-        "Violation": []
+        "Violation": [],
+        "Amount": []
         }
     
     return sheet
@@ -53,12 +54,55 @@ def excel_input():
 
     return sheet
 
-def export_excel():
-    df = pd.DataFrame(excel_input())
-    export_csv = df.to_csv("data.csv", index=False)
+def export_excel(table, name):
+    df = pd.DataFrame.from_dict(table, orient="index").transpose()
+    export_csv = df.to_csv(name, index=False)
 
+def entry_data_mod():
+    f = codecs.open("citemgr_2013_small.html", "r", "utf-8")
+    soup = bs(f.read(), "html.parser")
+    sheet = init_excel()
+    column = 0
+
+    for i in soup.find_all("td", class_="tblkeypcs"): # Cite ID
+        sheet["Cite ID"].append(i.a.get_text())
+
+    for i in soup.find_all("td", class_="tblpcs"): # cite#, date, license, owner, status
+        if column == 6:
+            column = 0
+
+        if column == 0:
+            sheet["Cite Number"].append(i.get_text())
+        elif column == 1:
+            sheet["Date Issued"].append(i.get_text())
+        elif column == 2:
+            sheet["License Number"].append(i.get_text())
+        elif column == 3:
+            sheet["Registered Owner"].append(i.get_text())
+        elif column == 4:
+            pass
+        elif column == 5:
+            sheet["Status"].append(i.get_text())
+
+        column += 1
     
+    #---------------------------------------------------------
+    # f2 = codecs.open("citemgr_ex1_transaction.html")
+    # soup2 = bs(f2.read(), "html.parser")
+
+    # violation = []
+    # viol_number = soup2.find("td", class_="tblpcs").get_text().split()[0]
+    # violation.append(viol_number)
+    # data.append(violation)
+
+    # for i in sheet:
+    #     print("{}: {}".format(i, len(sheet[i])))
+
+    return sheet
+
 
 if __name__ == "__main__":
-    export_excel()
+    export_excel(entry_data_mod(), "data_mult.csv")
+    # print(entry_data_mod())
+    # entry_data_mod()
     
