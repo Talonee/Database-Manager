3ae26e05f4dd17676dd7e7057facba1216f768cf
Talon Pollard <Talon.Pollard@vvc.edu>
2019-10-17 16:08:20 -0700

    Initiate finding State

--- work.py
+++ work.py
@@ -1,30 +1,10 @@
 from bs4 import BeautifulSoup as bs # webscrape
 import requests # websites
 import codecs # files
-import pandas as pd # excel sheets
+# import pandas as pd # excel sheets
 
 # request website
 # page = requests.get("http://citemgr/citemgr/violation_trans_main.php?cite_array=&cite_sysid=83813&cite_number=1306-25673")
-
-
-def entry_data():
-    f = codecs.open("citemgr_ex1.html", "r", "utf-8")
-    soup = bs(f.read(), "html.parser")
-
-    data = []
-    data.append(soup.find("td", class_="tblkeypcs").a.get_text()) # Cite ID
-    for i in soup.find_all("td", class_="tblpcs"): # cite#, date, license, owner, status
-        data.append(i.get_text())
-    
-    f2 = codecs.open("citemgr_ex1_transaction.html")
-    soup2 = bs(f2.read(), "html.parser")
-
-    violation = []
-    viol_number = soup2.find("td", class_="tblpcs").get_text().split()[0]
-    violation.append(viol_number)
-    data.append(violation)
-
-    return data
 
 def init_excel():
     sheet = {
@@ -35,23 +15,10 @@ def init_excel():
         "Registered Owner": [],
         "Status": [],
         "Violation": [],
-        "Amount": []
+        "Amount": [],
+        "State": []
         }
     
-    return sheet
-
-def excel_input():
-    sheet = init_excel()
-    data = entry_data()
-
-    sheet["Cite ID"].append(data[0])
-    sheet["Cite Number"].append(data[1])
-    sheet["Date Issued"].append(data[2])
-    sheet["License Number"].append(data[3])
-    sheet["Registered Owner"].append(data[4])
-    sheet["Status"].append(data[6])
-    sheet["Violation"].append(data[7])
-
     return sheet
 
 def export_excel(table, name):
@@ -61,6 +28,7 @@ def export_excel(table, name):
 def entry_data_mod():
     f = codecs.open("citemgr_2013_small.html", "r", "utf-8")
     soup = bs(f.read(), "html.parser")
+    
     sheet = init_excel()
     column = 0
 
@@ -86,6 +54,12 @@ def entry_data_mod():
 
         column += 1
     
+    for i in sheet["Cite ID"]:
+        url = "http://citemgr/citemgr/cite_edit.php?cite_sysid={}&username=".format(i)
+        page = requests.get(url)
+        soup2 = bs(page.content, "html.parser")
+
+
     #---------------------------------------------------------
     # f2 = codecs.open("citemgr_ex1_transaction.html")
     # soup2 = bs(f2.read(), "html.parser")
@@ -102,7 +76,7 @@ def entry_data_mod():
 
 
 if __name__ == "__main__":
-    export_excel(entry_data_mod(), "data_mult.csv")
-    # print(entry_data_mod())
+    # export_excel(entry_data_mod(), "data_mult.csv")
+    print(entry_data_mod())
     # entry_data_mod()
     
