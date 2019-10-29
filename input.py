113ad44a2dbca5c0914e6cc9728ef3e7aca2c06e
Talon Pollard <Talon.Pollard@vvc.edu>
2019-10-29 16:53:11 -0700

    Initial setup for input.py

--- input.py
+++ input.py
@@ -1,25 +1,30 @@
-import selenium
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
-# driver = webdriver.Chrome()
-
-# driver.get('https://victorvalley.parkadmin.com/admin/start/login.aro')
+driver = webdriver.Chrome()
+driver.get('https://victorvalley.parkadmin.com/admin/start/login.aro')
 
 with open("login.txt") as f:
     f = f.read().splitlines()
     un = f[0]
     pw = f[1]
 
-
 # print(driver.current_url)
 
-# id_box = driver.find_element_by_id("username")
-# id_box.send_keys(un)
+id_box = driver.find_element_by_id("username")
+id_box.send_keys(un)
 
-# pass_box = driver.find_element_by_id("password")
-# pass_box.send_keys(pw)
+pass_box = driver.find_element_by_id("password")
+pass_box.send_keys(pw)
 
-# login_button = driver.find_element_by_id("login")
-# login_button.click()
+login_button = driver.find_element_by_id("login")
+login_button.click()
 
-# print(driver.current_url)
+element = driver.find_element_by_xpath("/html/body/nav/div/div[2]/ul[1]/li[1]/a")
+hover = ActionChains(driver).move_to_element(element)
+hover.perform()
+
+driver.implicitly_wait(3)
+
+ay = driver.find_element_by_xpath("/html/body/nav/div/div[2]/ul[1]/li[1]/ul/li[3]/a")
+ay.click()
