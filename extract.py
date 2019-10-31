cd5385b3752e8ac336f497f236feb3ffc93417fc
Talon Pollard <Talon.Pollard@vvc.edu>
2019-10-31 15:25:08 -0700

    Update an address function for extract.py

--- extract.py
+++ extract.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup as bs  # webscrape
 import requests  # websites
 import pandas as pd  # excel sheets
 import time
+import datetime
 
 
 def database():
-    with open("Years/citemgr_1999.html", buffering=(2<<16) + 8) as f:
+    with open("Years/citemgr_1999.html", buffering=(2 << 16) + 8) as f:
         soup = bs(f.read(), "html.parser")
         cite_id = []
         citation = []
@@ -19,32 +20,32 @@ def database():
 
         [
             cite_id.append(i.a.text)
-            for i in soup.find_all("td", {"class":"tblkeypcs", "width":"65", "align":"center"})
+            for i in soup.find_all("td", {"class": "tblkeypcs", "width": "65", "align": "center"})
         ]  # Cite ID
 
         [
             citation.append(i.text)
-            for i in soup.find_all("td", {"align":"center", "width":"80", "class":"tblpcs"})
-        ] # Citation
+            for i in soup.find_all("td", {"align": "center", "width": "80", "class": "tblpcs"})
+        ]  # Citation
 
         [
             date.append(i.text)
-            for i in soup.find_all("td", {"align":"center", "width":"95", "class":"tblpcs"})
-        ] # Date
-        
+            for i in soup.find_all("td", {"align": "center", "width": "95", "class": "tblpcs"})
+        ]  # Date
+
         [
             plate.append(i.text)
-            for i in soup.find_all("td", {"align":"center", "width":"85", "class":"tblpcs"})
-        ] # Plate
+            for i in soup.find_all("td", {"align": "center", "width": "85", "class": "tblpcs"})
+        ]  # Plate
 
         [
             full_name.append(i.text)
-            for i in soup.find_all("td", {"align":"left", "class":"tblpcs"})
-        ] # Full name
+            for i in soup.find_all("td", {"align": "left", "class": "tblpcs"})
+        ]  # Full name
 
         # Status
         c = 0
-        for i in soup.find_all("td", {"align":"right", "width":"65", "class":"tblpcs"}):
+        for i in soup.find_all("td", {"align": "right", "width": "65", "class": "tblpcs"}):
             if c % 2 == 1:
                 status.append(i.text)
             c += 1
@@ -76,14 +77,14 @@ def database():
 def web():
     sheet = {}
     sheet["Cite ID"], sheet["Citation"], sheet["Date"], sheet["Plate"], sheet[
-        "Full Name"], sheet["First"], sheet["Middle"], sheet["Last"], sheet[
-            "Status"] = database()
+        "Full Name"], sheet["First"], sheet["Middle"], sheet["Last"], sheet["Status"] = database()
     sheet["State"] = []
     sheet["Violation"] = []
     sheet["Amount"] = []
 
     for id in sheet["Cite ID"]:  # State, amount, violation (web)
-        url = "http://citemgr/citemgr/cite_edit.php?cite_sysid={}&username=".format(id)
+        url = "http://citemgr/citemgr/cite_edit.php?cite_sysid={}&username=".format(
+            id)
         page = requests.get(url)
         soup2 = bs(page.content, "html.parser")
 
@@ -91,7 +92,8 @@ def web():
         sheet["State"].append(items)
 
         num = sheet["Citation"][sheet["Cite ID"].index(id)]
-        url = "http://citemgr/citemgr/violation_trans_main.php?cite_array=&cite_sysid={}&cite_number={}".format(id, num)
+        url = "http://citemgr/citemgr/violation_trans_main.php?cite_array=&cite_sysid={}&cite_number={}".format(
+            id, num)
         page = requests.get(url)
         soup2 = bs(page.content, "html.parser")
 
@@ -107,7 +109,7 @@ def web():
                           bgcolor="#8B6914").get_text()
         amnt_clean = "{}".format(amnt.split()[2])
         sheet["Amount"].append(amnt_clean)
-        
+
     return sheet
 
 
@@ -118,9 +120,11 @@ def export_excel(table, name):
         df = pd.DataFrame.from_dict(table, orient="index").transpose()
 
     df = df[[
-        "Cite ID", "Citation", "Date", "Plate", "State", "Full Name", "First",
-        "Middle", "Last", "Violation", "Amount", "Status", "Vehicle Make", 
-        "Vehicle Model", "Vehicle Color"
+        'Cite ID', 'Citation', 'Date', 'Plate', 'State', 'Full Name', 'First',
+       'Middle', 'Last', 'Violation', 'Amount', 'Status', 'Vehicle Make',
+       'Vehicle Model', 'Vehicle Color', 'Driver Name', 'Driver Address',
+       'Driver CSZ', 'Driver Phone', 'Owner Name', 'Co-Owner', 'Owner Address',
+       'Owner CSZ', 'Owner Phone'
     ]]
     export_csv = df.to_csv(name, index=False)
 
@@ -132,30 +136,76 @@ def vehicle_desc():
     color = []
 
     for i in df["Cite ID"]:
-        url = "http://citemgr/citemgr/cite_edit.php?cite_sysid={}&username=".format(i)
+        url = "http://citemgr/citemgr/cite_edit.php?cite_sysid={}&username=".format(
+            i)
         page = requests.get(url)
         soup2 = bs(page.content, "html.parser")
 
-        item1 = soup2.find('input', {'name': 'vehicle_make'}).get('value')
-        make.append(item1)
-
-        item2 = soup2.find('input', {'name': 'vehicle_model'}).get('value')
-        model.append(item2)
-
-        item3 = soup2.find('input', {'name': 'vehicle_color'}).get('value')
-        color.append(item3)
+        make.append(soup2.find('input', {'name': 'vehicle_make'}).get('value'))
+        model.append(soup2.find(
+            'input', {'name': 'vehicle_model'}).get('value'))
+        color.append(soup2.find(
+            'input', {'name': 'vehicle_color'}).get('value'))
 
     df["Vehicle Make"] = make
     df["Vehicle Model"] = model
     df["Vehicle Color"] = color
-    
+
     return df
 
-if __name__ == "__main__":    
+
+def address():
+    df = pd.read_csv("Output v2/Copy of 2013 DATA.csv")
+    dvr = []
+    dvr_addy = []
+    dvr_city = []
+    dvr_fone = []
+    own = []
+    coown = []
+    own_addy = []
+    own_city = []
+    own_fone = []
+
+    for i in df["Cite ID"]:
+        url = "http://citemgr/citemgr/cite_edit.php?cite_sysid={}&username=".format(i)
+        page = requests.get(url)
+        soup2 = bs(page.content, "html.parser")
+
+        dvr.append(soup2.find('input', {'name': 'driver_name'}).get('value'))
+        dvr_addy.append(soup2.find(
+            'input', {'name': 'driver_addr'}).get('value'))
+        dvr_city.append(soup2.find(
+            'input', {'name': 'driver_csz'}).get('value'))
+        dvr_fone.append(soup2.find(
+            'input', {'name': 'driver_phone'}).get('value'))
+        own.append(soup2.find('input', {'name': 'owner_name'}).get('value'))
+        coown.append(soup2.find('input', {'name': 'owner_name2'}).get('value'))
+        own_addy.append(soup2.find(
+            'input', {'name': 'owner_addr'}).get('value'))
+        own_city.append(soup2.find(
+            'input', {'name': 'owner_csz'}).get('value'))
+        own_fone.append(soup2.find(
+            'input', {'name': 'owner_phone'}).get('value'))
+
+    df["Driver Name"] = dvr
+    df["Driver Address"] = dvr_addy
+    df["Driver CSZ"] = dvr_city
+    df["Driver Phone"] = dvr_fone
+    df["Owner Name"] = own
+    df["Co-Owner"] = coown
+    df["Owner Address"] = own_addy
+    df["Owner CSZ"] = own_city
+    df["Owner Phone"] = own_fone
+
+    return df
+
+
+if __name__ == "__main__":
     # start = time.time()
     # export_excel(web(), "Copy of 1999.csv")
     # print("Time: {}".format(time.time()-start))
+
     start = time.time()
-    export_excel(vehicle_desc(), "Copy of 1999 DATA.csv")
-    print("Time: {}".format(time.time()-start))
-    
+    export_excel(address(), "Output v3/Copy of 2013 DATA.csv")
+    sec = time.time()-start
+    print("Time: {}".format(str(datetime.timedelta(seconds=sec))))
